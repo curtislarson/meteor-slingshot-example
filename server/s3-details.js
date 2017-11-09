7c077b215e00dc1962a0dd2897a63f43ca960af2
Daniel Meechan <daniel.meechan@ibm.com>
2017-11-09 11:16:10 +0000

    Added error throw if unable to find S3 details

--- server/s3-details.js
+++ server/s3-details.js
@@ -1,9 +1,11 @@
+import { Meteor } from 'meteor/meteor'
+
 const s3Conf = Meteor.settings.s3 || {};
 
-if (s3Conf && s3Conf.key && s3Conf.secret && s3Conf.bucket && s3Conf.region) {
-    console.log('Found AWS S3 details');
-} else {
-    console.error('Error: unable to find AWS S3 details in settings.json');
+if (!(s3Conf && s3Conf.key && s3Conf.secret && s3Conf.bucket && s3Conf.region)) {
+    console.error('s3-details.js | Unable to fetch AWS S3 details from settings.json');
+    console.error('s3-details.js | Make sure settings.json exists and use "meteor --settings settings.json" to run Meteor')
+    throw new Meteor.Error('missing-s3-settings', 's3-details.js | Unable to fetch AWS S3 details from settings.json');
 }
 
 export { s3Conf };
